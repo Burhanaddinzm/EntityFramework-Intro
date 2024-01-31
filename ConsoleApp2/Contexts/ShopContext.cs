6db6ede8101923efab6fba16af692fabdf6a1df1
Mirsadizada Burhanaddin <burhanaddinzm@code.local>
2024-01-31 17:19:47 +0400

    Entity Framework Intro

---
+++ ConsoleApp2/Contexts/ShopContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ConsoleApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Contexts
+{
+    public class ShopContext : DbContext
+    {
+        private string connectionString = "Server=SUN01;Database=Shop;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp2.Contexts;
+using ConsoleApp2.Models;
+
+ShopContext shopContext = new ShopContext();
+void GetAll()
+{
+    List<Category> categories = shopContext.Category.Where(cat => !cat.IsDeleted).ToList();
+
+    foreach (Category category in categories)
+        Console.WriteLine($"Id:{category.Id} Name:{category.Name} IsDeleted:{category.IsDeleted}");
+}
+
+void GetById()
+{
+    Console.WriteLine("Input Id:");
+    int.TryParse(Console.ReadLine(), out int Id);
+
+    Category? category = shopContext.Category.Where(cat => cat.Id == Id && !cat.IsDeleted).FirstOrDefault();
+
+    if (category != null)
+        Console.WriteLine($"Id:{category.Id} Name:{category.Name} IsDeleted:{category.IsDeleted}");
+    else
+        Console.WriteLine("Not Found!");
+}
+
+void Create()
+{
+    Console.WriteLine("Input Name:");
+    string Name = Console.ReadLine();
+
+    Category category = new Category();
+    category.Name = Name;
+    shopContext.Add(category);
+    int result = shopContext.SaveChanges();
+
+    if (result == 0)
+        Console.WriteLine("Failed to save changes!");
+    else
+        Console.WriteLine("Saved Changes!");
+}
+
+Create();
+GetById();
+GetAll();
